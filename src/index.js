d327fff58c7e031fc77c35a812301a2007e15554
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-11-09 19:54:43 +0100

    catch some gdpr related stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const conditions = ['terms', 'policy', 'prvacy', 'privacy'];
-const marketing = ['marketing', 'receive', 'send me', 'email', 'sms', 'news', 'updates', 'offer', 'advertising', 'analytics'];
+const marketing = ['marketing', 'receive', 'send me', 'email', 'sms', 'news', 'updates', 'offer', 'advertising', 'analytics', 'tailored'];
 const optOut = ['do not', 'don\'t'];
 
 const isTermsAndConditions = text => typeof text === 'string' && conditions.some(term => text.includes(term));
