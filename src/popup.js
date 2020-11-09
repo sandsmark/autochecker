888fbde198298409f10a3f5f7b283686d542cd6f
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-11-09 19:55:19 +0100

    ugly code but a bit more easily readable text

--- src/popup.js
+++ src/popup.js
@@ -12,28 +12,55 @@ const generateHeader = (operations = []) => {
   container.appendChild(headerElement);
 };
 
-const generateCheckboxInfo = (operations = []) => {
-  const container = document.getElementById('autochecker-results');
-  if (operations.length) {
-    operations.forEach((operation) => {
+function createElement(container, operation) {
       const labelTextElement = document.createElement('p');
       // add an italics element for displaying the label text of the checkbox
       const italicsElement = document.createElement('i');
       const boldElement = document.createElement('b');
+      const infoText = document.createTextNode(`${operation.category} ` + (operation.checkboxId !== '' ? ` (${operation.checkboxId}) ` : ''));
       const actionText = document.createTextNode(`${operation.action}: `);
       boldElement.appendChild(actionText);
+      italicsElement.appendChild(infoText);
       italicsElement.appendChild(boldElement);
       const labelText = document.createTextNode(`"${operation.text}"`);
       italicsElement.appendChild(labelText);
       labelTextElement.appendChild(italicsElement);
       container.appendChild(labelTextElement);
       // now add the message from auto-checker
-      const infoElement = document.createElement('p');
-      const infoText = document.createTextNode(`The above text (${operation.checkboxId}) was identified as ${operation.category}. \
-The checkbox has been ${operation.action}.`);
-      infoElement.appendChild(infoText);
-      container.appendChild(infoElement);
-      container.appendChild(document.createElement('hr'));
+      //const infoElement = document.createElement('p');
+      //infoElement.appendChild(infoText);
+      //container.appendChild(infoElement);
+      //container.appendChild(document.createElement('hr'));
+}
+
+const generateCheckboxInfo = (operations = []) => {
+  const container = document.getElementById('autochecker-results');
+  if (operations.length) {
+    // Probably the worst way to do it
+
+    const checkedHeader = document.createElement('h2');
+    checkedHeader.appendChild(document.createTextNode('Checked'));
+    container.appendChild(checkedHeader);
+    operations.forEach((operation) => {
+      if (operation.action === 'checked') {
+        createElement(container, operation);
+      }
+    });
+    const uncheckedHeader = document.createElement('h2');
+    uncheckedHeader.appendChild(document.createTextNode('Unchecked'));
+    container.appendChild(uncheckedHeader);
+    operations.forEach((operation) => {
+      if (operation.action === 'unchecked') {
+        createElement(container, operation);
+      }
+    });
+    const ignoredHeader = document.createElement('h2');
+    ignoredHeader.appendChild(document.createTextNode('Ignored'));
+    container.appendChild(ignoredHeader);
+    operations.forEach((operation) => {
+      if (operation.action === 'ignored') {
+        createElement(container, operation);
+      }
     });
   } else {
     const infoContainer = document.createElement('p');
